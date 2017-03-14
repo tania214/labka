8a799c2df911eeecb39dc605b8b83b342a3cd5c8
tania214 <Tanushatko@ukr.net>
2017-03-14 19:25:03 +0200

    first work

---
+++ src/first/Example.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Example {
+	
+	public static void main (String args []) {
+		int a=32;
+		int b=13;
+		int c;
+		c=a*a+b*b;
+		System.out.println("Sum of squares of two numbers: a="+a+", "+"b="+b);
+		System.out.println("wil be: c="+c);
+		
+	}
+
+}
